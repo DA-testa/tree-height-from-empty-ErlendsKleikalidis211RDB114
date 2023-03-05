8ed3ddf5638e263d5e39cceee463a50afb8b1064
ErlendsKleikalidis211RDB114 <125798243+ErlendsKleikalidis211RDB114@users.noreply.github.com>
2023-03-05 22:14:24 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,18 +1,26 @@
+class Node:
+    def __init__(self, index):
+        self.index = index
+        self.children = []
+
+def compute_height(n, parents):
+    nodes = [Node(i) for i in range(n)]
+    root = None
+    for i, parent_index in enumerate(parents):
+        if parent_index == -1:
+            root = nodes[i]
+        else:
+            parent_node = nodes[parent_index]
+            parent_node.children.append(nodes[i])
+    
+    def get_height(node):
+        if not node.children:
+            return 1
+        else:
+            return 1 + max(get_height(child) for child in node.children)
+    
+    return get_height(root)
+
 n = int(input())
 parents = list(map(int, input().split()))
-
-tree = [[] for i in range(n)]
-for i in range(n):
-    if parents[i] == -1:
-        root = i
-    else:
-        tree[parents[i]].append(i)
-
-def compute_height(node):
-    if not tree[node]:
-        return 1
-    else:
-        heights = [compute_height(child) for child in tree[node]]
-        return 1 + max(heights)
-
-print(compute_height(root))
+print(compute_height(n, parents))
